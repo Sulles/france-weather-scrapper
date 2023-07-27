237ec9553880daa67bc270df2e367ec6019ab0f3
dashielljarvis <64041977+dashielljarvis@users.noreply.github.com>
2023-07-27 18:11:28 +0200

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,42 +1,28 @@
-#take list of temps from suley
-temps_list = [10.8,
-10.8,
-11.9,
-13.5,
-13.9,
-15,
-17,
-15.9,
-19.1,
-17.8,
-14.4,
-18.5,
-15.8,
-15.3,
-14.4,
-12.2,
-12.1,
-12.8,
-12.6,
-12.4,
-11.9,
-11.5,
-11.9,
-12]
+#function to calculate hot and cold stitches given a list. Returns two ints
+def calculate_hot_cold_stitches(temps_list):
+    # Calculate average temperature
+    avg_temp = sum(temps_list) / len(temps_list)
+    
+    #loop through temps to find how entries are hotter than average
+    hotter_than_avg_counter = 0
+    for temp in temps_list:
+        if temp >= avg_temp:
+            hotter_than_avg_counter += 1
 
-#get average temp
-avg_temp = sum(temps_list) / len(temps_list)
+    #calc hot and cold stitches
+    hot_stitches = round(hotter_than_avg_counter / len(temps_list) * 250)
+    cold_stitches = 250 - hot_stitches
+    
+    return hot_stitches, cold_stitches
 
-#loop through temps to find how entries are hotter than average
-hotter_than_avg_counter = 0
-for temp in temps_list:
-    if temp >= avg_temp:
-        hotter_than_avg_counter += 1
+# Given list of temperatures
+temps_list = [10.8, 10.8, 11.9, 13.5, 13.9, 15, 17, 15.9, 19.1, 17.8,
+              14.4, 18.5, 15.8, 15.3, 14.4, 12.2, 12.1, 12.8, 12.6,
+              12.4, 11.9, 11.5, 11.9, 12]
 
-#calc hot and cold stitches
-hot_stitches = round(hotter_than_avg_counter / len(temps_list) * 250)
-cold_stitches = 250 - hot_stitches
+# Calculate hot and cold stitches
+hot_stitches, cold_stitches = calculate_hot_cold_stitches(temps_list)
 
-#print output
-print('Hot stitches: ', hot_stitches)
-print('Cold stitches: ', cold_stitches)
+# Print output
+print('Hot stitches:', hot_stitches)
+print('Cold stitches:', cold_stitches)
