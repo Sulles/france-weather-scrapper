9f534c769c0ecc791bb8d7b4f3f2c9090d378183
Sulles <suleymanbarthesukhera@gmail.com>
2023-07-28 10:28:30 +0200

    add comments

--- util.py
+++ util.py
@@ -2,13 +2,18 @@ import datetime
 import subprocess
 
 
-def date_from_day_month_year(str_vals: str):
-    day_month_year: list(int) = [int(_) for _ in str_vals.split('_')]
-    year_month_day = day_month_year[::-1]
-    return datetime.date(year_month_day[0], year_month_day[1], year_month_day[2])
+def dd_mm_yyyy2date(dd_mm_yyyy: str):
+    ''' convert dd_mm_yyyy string to datetime.date '''
+    # separate day, month, and year
+    dd_mm_yyyy: list(int) = [int(_) for _ in dd_mm_yyyy.split('_')]
+    # invert to datetime.date format of year, month, day
+    yyyy_mm_dd = dd_mm_yyyy[::-1]
+    # return datetime.date object
+    return datetime.date(yyyy_mm_dd[0], yyyy_mm_dd[1], yyyy_mm_dd[2])
 
 
 def frequency_complete(first_letter: str):
+    ''' verbose frequency, convert... m -> monthly, d -> daily, h -> hourly '''
     frequency_dict = dict(m="monthly", d="daily", h="hourly")
     return frequency_dict[first_letter]
 
