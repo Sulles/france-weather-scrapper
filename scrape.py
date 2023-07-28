0f9ff31c4b590ed3e31fde69eeb773f4ad039e3e
Sulles <suleymanbarthesukhera@gmail.com>
2023-07-28 10:29:19 +0200

    func update, usage description update

--- scrape.py
+++ scrape.py
@@ -2,18 +2,18 @@ import argparse
 import sqlite3
 import sys
 
-from util import date_from_day_month_year, frequency_complete
+from util import dd_mm_yyyy2date, frequency_complete
 
 
 parser = argparse.ArgumentParser(
     "scrape.py",
-    "scrape.py -s dd_mm_yyyy -e dd_mm_yyyy -f m/d/h",
+    "scrape.py -s [dd_mm_yyyy] -e [dd_mm_yyyy] -f [m, d, or h]",
     description="Scrape historic weather data from https://www.infoclimat.fr/observations-meteo/archives")
 parser.add_argument(
-    "-s", "--start", required=True, dest="start", metavar="start_date", type=date_from_day_month_year,
+    "-s", "--start", required=True, dest="start", metavar="start_date", type=dd_mm_yyyy2date,
     help="start date as dd_mm_yyyy")
 parser.add_argument(
-    "-e", "--end", required=True, dest="end", metavar="end_date", type=date_from_day_month_year,
+    "-e", "--end", required=True, dest="end", metavar="end_date", type=dd_mm_yyyy2date,
     help="end date as dd_mm_yyyy")
 parser.add_argument(
     "-f", "--freq", required=True, dest="freq", metavar="frequency", type=frequency_complete,
@@ -22,8 +22,7 @@ parser.add_argument(
 
 def main():
     args = parser.parse_args()
-    print(args)
-    print(type(args))
+    print(args.start)
 
 
 if __name__ == "__main__":
