79f5b37229c3e17ca1f59ba7e5c204194542bada
Sulles <suleymanbarthesukhera@gmail.com>
2023-08-03 20:29:58 +0200

    create config.py to store globally referenced variables

--- scrape.py
+++ scrape.py
@@ -5,14 +5,11 @@ import re
 import requests
 import time
 
+from config import *
 from util import *
 
 
-# WARNING: the first day of the month is "1er", not "1", all other days are their int value
-TARGET_SITE_ROOT = "https://www.infoclimat.fr/observations-meteo/archives"
-TARGET_SITE_TAIL = "albi-le-sequestre/07632.html"
 TIME_DELTA = datetime.timedelta(1.0)
-CSV_FILE_NAME = "raw_data.csv"
 
 
 parser = argparse.ArgumentParser(
