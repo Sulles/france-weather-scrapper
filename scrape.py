ca477359081162d2432361a80a469feb86ecf227
Sulles <suleymanbarthesukhera@gmail.com>
2023-08-11 15:35:16 +0200

    default end time is yesterday, not today
    this guarantees getting historic data, and not predicted data

--- scrape.py
+++ scrape.py
@@ -15,14 +15,13 @@ TIME_DELTA = datetime.timedelta(1.0)
 parser = argparse.ArgumentParser(
     "scrape.py",
     "scrape.py -s [dd_mm_yyyy] -e [dd_mm_yyyy] -f [m, d, or h]",
-    description=
-    "Scrape historic weather data from https://www.infoclimat.fr/observations-meteo/archives")
+    description="Scrape historic weather data from https://www.infoclimat.fr/observations-meteo/archives")
 parser.add_argument(
     "-s", "--start", required=True, dest="start", metavar="start_date", type=dd_mm_yyyy2date,
     help="start date as dd_mm_yyyy")
 parser.add_argument(
-    "-e", "--end", default=datetime.datetime.now().date(), dest="end", metavar="end_date",
-    type=dd_mm_yyyy2date, help="end date as dd_mm_yyyy")
+    "-e", "--end", default=datetime.datetime.now().date() - TIME_DELTA,
+    dest="end", metavar="end_date", type=dd_mm_yyyy2date, help="end date as dd_mm_yyyy")
 parser.add_argument(
     "-f", "--freq", default="hourly", dest="freq", metavar="frequency", choices=['daily', 'hourly'],
     type=frequency_complete, help="frequency as h (neither d nor m are supported!)")
